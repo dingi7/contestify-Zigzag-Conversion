8ebfaecbe583adf6ad0d879f681fa8ca7b33a9bd
Kamen Kanev <64739378+dingi7@users.noreply.github.com>
2024-11-30 11:54:22 +0200

    Update ZigzagConversion.js

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -1,5 +1,17 @@
 function ZigzagConversion(s, numRows){
+if (numRows === 1) return s;
 
+    const rows = new Array(Math.min(numRows, s.length)).fill('');
+    let curRow = 0;
+    let goingDown = false;
+
+    for (let c of s) {
+        rows[curRow] += c;
+        if (curRow === 0 || curRow === numRows - 1) goingDown = !goingDown;
+        curRow += goingDown ? 1 : -1;
+    }
+
+    return rows.join('');
 }
 
 module.exports = ZigzagConversion;
